e00c50779c81e6e397a7106ebe76c82cc5b6f24f
Anelizi <anelizicarvalho@gmail.com>
2023-05-03 21:00:15 -0300

    fixing: POST and Get /poll

--- src/controllers/polls.controllers.js
+++ src/controllers/polls.controllers.js
@@ -2,34 +2,34 @@ import dayjs from "dayjs";
 import { db } from "../database/database.connection.js";
 
 export async function pollPost(req, res) {
-  const { title } = req.body;
-  let { expireAt } = req.body;
+  const { title, expireAt } = req.body;
+
+  let expireDate;
 
   if (expireAt === "") {
-    expireAt = dayjs().add(30, "day").format("YYYY-MM-DD HH:mm:ss");
+    expireDate = dayjs().add(30, "day").format("YYYY-MM-DD HH:mm:ss");
   }
 
-  const poll = {
-    title,
-    expireAt,
-  };
-
   try {
-    await db.collection("polls").insertOne(poll);
+    const result = await db.collection("polls").insertOne({
+      title,
+      expireAt: expireDate,
+    });
 
-    res.status(201).send("Tudo certo");
+    res.sendStatus(201);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
 
 export async function pollGet(req, res) {
-  const { title } = req.body;
   try {
-    const polls = await db.collection("polls").find({ title }).toArray();
+    const polls = await db.collection("polls").find({}).toArray();
 
     res.status(200).send(polls);
   } catch (error) {
     res.status(500).send(error);
   }
 }
+
+
--- src/schemas/polls.schema.js
+++ src/schemas/polls.schema.js
@@ -5,6 +5,6 @@ import JoiDate from "@joi/date";
 const joi = BaseJoi.extend(JoiDate);
 
 export const pollSchema = Joi.object({
-  title: Joi.string().min(3).required(),
+  title: Joi.string().min(1).required(),
   expireAt: joi.date().format("YYYY-MM-DD HH:mm:ss").allow("").required(),
 });
