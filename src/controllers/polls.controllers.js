9956cf489c718fea98294598c3dfedff2e310a92
Anelizi <anelizicarvalho@gmail.com>
2023-05-05 20:04:24 -0300

    request: GET /poll/:id/choice

--- src/controllers/polls.controllers.js
+++ src/controllers/polls.controllers.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { ObjectId } from "mongodb";
 import { db } from "../database/database.connection.js";
 
 export async function pollPost(req, res) {
@@ -32,6 +33,24 @@ export async function pollGet(req, res) {
   }
 }
 
-export async function pollChoiceGet(req, res){}
+export async function pollChoiceGet(req, res) {
+  const { id } = req.params;
+  try {
+    const pollExist = await db
+      .collection("polls")
+      .findOne({ _id: new ObjectId(id) });
 
-export async function pollResultGet(req, res){}
+    if (!pollExist) return res.status(404).send("Enquete não existe");
+
+    const choices = await db
+      .collection("choices")
+      .find({ pollId: new ObjectId(id) })
+      .toArray();
+
+    res.status(200).send(choices);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+export async function pollResultGet(req, res) {}
